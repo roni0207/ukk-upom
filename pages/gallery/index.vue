045886fe11a759cdab43d73dbe6d25e51016ebd9
roni0207 <141709439+roni0207@users.noreply.github.com>
2025-01-05 19:46:58 +0700

    Add files via upload

---
+++ pages/gallery/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container text-center">
+        <div>
+            <h1 class="p-4">GALLERY SMKN 4 TASIKMALAYA</h1>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="/assets/gallery/g1.jpg" class="card-img-top" alt="...">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="/assets/gallery/g2.jpg" class="card-img-top" alt="...">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="/assets/gallery/g3.jpg" class="card-img-top" alt="...">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
